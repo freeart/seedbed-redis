2c5f78b7bef4f4fb524d9149ceab28f2193a86be
Pavel Nekrasov <pavel@resultdevelopment.com>
2019-02-22 12:03:36 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,29 @@
+const assert = require('assert'),
+	redis = require("redis")
+
+module.exports = function () {
+	assert(!this.redis, "field exists")
+
+	this.redis = redis.createClient(Object.assign(this.config.get("redis.connection"), {
+		retry_strategy: function (options) {
+			if (options.error && options.error.code === 'ECONNREFUSED') {
+				// End reconnecting on a specific error and flush all commands with
+				// a individual error
+				return new Error('The server refused the connection');
+			}
+			if (options.total_retry_time > 1000 * 60 * 60) {
+				// End reconnecting after a specific timeout and flush all commands
+				// with a individual error
+				return new Error('Retry time exhausted');
+			}
+			if (options.attempt > 10) {
+				// End reconnecting with built in error
+				return undefined;
+			}
+			// reconnect after
+			return Math.min(options.attempt * 100, 3000);
+		}
+	}));
+
+	return Promise.resolve();
+}
