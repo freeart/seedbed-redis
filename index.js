576194a3e08bc297ad1406cffc41ad95fcd2822e
Pavel Nekrasov <pavel@resultdevelopment.com>
2019-02-28 18:58:04 +0100

    added multi instance

--- index.js
+++ index.js
@@ -4,26 +4,29 @@ const assert = require('assert'),
 module.exports = function () {
 	assert(!this.redis, "field exists")
 
-	this.redis = redis.createClient(Object.assign({
-		retry_strategy: function (options) {
-			if (options.error && options.error.code === 'ECONNREFUSED') {
-				// End reconnecting on a specific error and flush all commands with
-				// a individual error
-				return new Error('The server refused the connection');
+	this.getRedisInstance = () => {
+		return redis.createClient(Object.assign({
+			retry_strategy: function (options) {
+				if (options.error && options.error.code === 'ECONNREFUSED') {
+					// End reconnecting on a specific error and flush all commands with
+					// a individual error
+					return new Error('The server refused the connection');
+				}
+				if (options.total_retry_time > 1000 * 60 * 60) {
+					// End reconnecting after a specific timeout and flush all commands
+					// with a individual error
+					return new Error('Retry time exhausted');
+				}
+				if (options.attempt > 10) {
+					// End reconnecting with built in error
+					return undefined;
+				}
+				// reconnect after
+				return Math.min(options.attempt * 100, 3000);
 			}
-			if (options.total_retry_time > 1000 * 60 * 60) {
-				// End reconnecting after a specific timeout and flush all commands
-				// with a individual error
-				return new Error('Retry time exhausted');
-			}
-			if (options.attempt > 10) {
-				// End reconnecting with built in error
-				return undefined;
-			}
-			// reconnect after
-			return Math.min(options.attempt * 100, 3000);
-		}
-	}, this.config.get("redis")));
+		}, this.config.get("redis")));
+	}
+	this.redis = this.getInstance()
 
 	this.redis.on("error", function (err) {
 		console.error(err);
